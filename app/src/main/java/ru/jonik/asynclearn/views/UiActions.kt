32799ead1c60ffa27e1d77b93115e3dbf5764752
JonikBoltaev <boltaevjahongiral@gmail.com>
2022-04-09 23:24:35 +0300

    Views setting

--- app/src/main/java/ru/jonik/asynclearn/views/UiActions.kt
+++ app/src/main/java/ru/jonik/asynclearn/views/UiActions.kt
@@ -2,14 +2,8 @@ package ru.jonik.asynclearn.views
 
 interface UiActions {
 
-    /**
-     * Display a simple toast message.
-     */
     fun toast(message: String)
 
-    /**
-     * Get string resource content by its identifier.
-     */
     fun getString(messageRes: Int, vararg args: Any): String
 
 }
