d07abf14b37b0d43d15264bbcb5796a5d0d03e93
JonikBoltaev <boltaevjahongiral@gmail.com>
2022-04-09 23:25:32 +0300

    Добавлены комментарии

--- app/src/main/java/ru/jonik/asynclearn/views/Navigator.kt
+++ app/src/main/java/ru/jonik/asynclearn/views/Navigator.kt
@@ -4,15 +4,7 @@ package ru.jonik.asynclearn.views
  * Navigation for your application
  */
 interface Navigator {
-
-    /**
-     * Launch a new screen at the top of back stack.
-     */
     fun launch(screen: BaseScreen)
-
-    /**
-     * Go back to the previous screen and optionally send some results.
-     */
     fun goBack(result: Any? = null)
 
 }
